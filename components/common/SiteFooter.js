90631e36f3cd96f18c65bf667cb6a440cd23afe4
dalespiteri <dalespiteri@gmail.com>
2021-05-05 16:11:49 -0400

    completely clear footer

--- components/common/SiteFooter.js
+++ components/common/SiteFooter.js
@@ -1,10 +1,4 @@
 import React from "react";
-import Image from "next/image";
-import { FaTwitter, FaInstagram, FaSlack, FaYoutube } from "react-icons/fa";
-
-/**
- * This footer is not part of the content in the CMS, feel free to remove this for production use.
- */
 
 const SiteFooter = () => {
 
