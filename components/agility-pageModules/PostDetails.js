6ecdd75f8d591a315709fbe10ea2f2c0c441ffcb
dalespiteri <dalespiteri@gmail.com>
2021-05-06 16:14:48 -0400

    clear out tw

--- components/agility-pageModules/PostDetails.js
+++ components/agility-pageModules/PostDetails.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { renderHTML } from "@agility/nextjs";
-import Image from "next/image";
 
 const PostDetails = ({ dynamicPageItem }) => {
   // post fields
@@ -13,28 +12,25 @@ const PostDetails = ({ dynamicPageItem }) => {
   const dateStr = new Date(post.date).toLocaleDateString();
 
   return (
-    <div className="relative px-8">
-      <div className="max-w-screen-xl mx-auto">
-        <div className="h-64 md:h-96 relative">
-          <Image
+    <div>
+      <div>
+        <div>
+          <img
             src={post.image.url}
-            className="object-cover object-center rounded-lg"
-            layout="fill"
           />
         </div>
-        <div className="max-w-2xl mx-auto mt-4">
-          <div className="uppercase text-primary-500 text-xs font-bold tracking-widest leading-loose">
+        <div>
+          <div>
             {category}
           </div>
-          <div className="border-b-2 border-primary-500 w-8"></div>
-          <div className="mt-4 uppercase text-gray-600 italic font-semibold text-xs">
+          <div></div>
+          <div>
             {dateStr}
           </div>
-          <h1 className="font-display text-4xl font-bold my-6 text-secondary-500">
+          <h1>
             {post.title}
           </h1>
           <div
-            className="prose max-w-full mb-20"
             dangerouslySetInnerHTML={renderHTML(post.content)}
           />
         </div>
